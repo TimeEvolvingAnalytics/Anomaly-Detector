bbc8dc1102ab987c8dda865a490613685c2139ec
gziffer <giacomo.ziffer@gmail.com>
2021-12-16 18:47:34 +0100

    api data response

--- anomaly_detector/main.py
+++ anomaly_detector/main.py
@@ -1,8 +1,12 @@
 from flask import Flask
 from flask import request
+import pandas as pd
+import json
+from pathlib import Path
 
 app = Flask(__name__)
 
+_STATUS = 200
 
 @app.route("/")
 def hello_world():
@@ -11,35 +15,63 @@ def hello_world():
 
 @app.route("/status", methods=['GET'])
 def get_status():
-    return 200
+    return 'The status of the Anomaly Detector is ' + str(_STATUS)
 
 
 @app.route("/<measurement>/stats", methods=['GET'])
 def get_measurement_stats(measurement):
-    # dataset/stats/<measurement>.csv
-    # colonna hash + campi che compongono l'hash (split per _)
-    return 200
+    path = 'anomaly_detector/dataset/stats/'+measurement+'.csv'
+    try:
+        abs_path = Path(path).resolve(strict=True)
+    except FileNotFoundError:
+        return "<p>"+measurement+": Invalid measurement."
+    else:
+        columns = ['dst','dstp','proto','service','src','srcp','url']
+        data = pd.read_csv(path)
+        data[columns] = data['hash_table'].str.split('_', expand=True)
+        data.drop(columns=['avg_diff','std_diff','cnt_diff','ss_diff'], inplace=True, axis=1)
+        # encode hash_table
+        data['hash_table'] = data['hash_table'].apply(lambda row: row.encode('utf8').hex())
+        return data.to_html()
 
 
 @app.route("/<measurement>/stats/<hash>", methods=['GET'])
 def get_measurement_stats_hash(measurement, hash):
-    # dataset/stats/<measurement>.csv
-    # ritorno le statistiche corrispondenti all'hash
-    return 200
+    path = 'anomaly_detector/dataset/stats/'+measurement+'.csv'
+    try:
+        abs_path = Path(path).resolve(strict=True)
+    except FileNotFoundError:
+        return "<p>"+measurement+": Invalid measurement."
+    else:
+        # decode hash_table
+        hash_decoded = bytes.fromhex(hash).decode('utf-8')
+        data = pd.read_csv(path)
+        data = data[data['hash_table']==hash_decoded]
+        return data.to_html()
 
 
 @app.route("/<entity_type>", methods=['GET'])
 def get_entity_type(entity_type):
-    # dataset/lists/<entity_type>.csv
-    # ritorno il dataset, se non c'è ritorno errore
-    return 200
+    path = 'anomaly_detector/dataset/lists/'+entity_type+'.csv'
+    try:
+        abs_path = Path(path).resolve(strict=True)
+    except FileNotFoundError:
+        return "<p>"+entity_type+": Invalid Entity."
+    else:
+        data = pd.read_csv(path)
+        return data.to_html()
 
 
 @app.route("/<measurement>/anomalies", methods=['GET'])
 def get_measurement_anomalies(measurement):
-    # dataset/anomalies/<measurement>.csv
-    # ritorno il dataset, se non c'è ritorno errore
-    return 200
+    path = 'anomaly_detector/dataset/anomalies/' + measurement + '.csv'
+    try:
+        abs_path = Path(path).resolve(strict=True)
+    except FileNotFoundError:
+        return "<p>" + measurement + ": Invalid Entity."
+    else:
+        data = pd.read_csv(path)
+        return data.to_html()
 
 
 @app.route("/<measurement>/anomalies/<uuid>", methods=['POST'])
